c52e63192adb3251d65d7344af1728562b65940d
prakhardg <prakhar4msoffice@gmail.com>
2019-06-19 14:28:31 +0530

    changed default db to postgresql

--- django_psql/settings.py
+++ django_psql/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'psql_app',
 ]
 
 MIDDLEWARE = [
@@ -73,13 +74,18 @@ WSGI_APPLICATION = 'django_psql.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'articledb',
+        'USER': 'postgres',
+        'PASSWORD': 'prakhardg',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
-}
 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
--- psql_app/models.py
+++ psql_app/models.py
@@ -6,6 +6,10 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    rating = models.DecimalField()
+    rating = models.DecimalField(max_digits=10, decimal_places=4)
     times_read = models.IntegerField()
     published_on = models.DateTimeField(auto_now=True, auto_now_add=False)
+    readkey = models.TextField()
+
+
+
